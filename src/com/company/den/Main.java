6fc87e3e1d84d58e61a089a1a2408fe8ccd59a11
Carl -F. Schulze <carl-friedrich.schulze@den-online.de>
2019-10-14 11:58:10 +0200

    verbesserung übersichtlichkeit

--- src/com/company/den/Main.java
+++ src/com/company/den/Main.java
@@ -30,31 +30,31 @@ public class Main {
             String csv1_spalte1 = scanner.nextLine();
             System.out.println(csv1_spalte1 + " Spalte soll bei: " + datei2 + " mit ... verglichen werden: ");
             String csv2_spalte1 = scanner.nextLine();
-
+            System.out.println("-----------------------------------------------------------------------------");
             System.out.println("Welche Spalte soll bei: " + datei1 + " verglichen werden?");
             String csv1_spalte2 = scanner.nextLine();
             System.out.println(csv1_spalte2 + " Spalte soll bei: " + datei2 + " mit ... verglichen werden: ");
             String csv2_spalte2 = scanner.nextLine();
-
+            System.out.println("-----------------------------------------------------------------------------");
             System.out.println("Welche Spalte soll bei: " + datei1 + " verglichen werden?");
             String csv1_spalte3 = scanner.nextLine();
             System.out.println(csv1_spalte3 + " Spalte soll bei: " + datei2 + " mit ... verglichen werden: ");
             String csv2_spalte3 = scanner.nextLine();
-
+            System.out.println("-----------------------------------------------------------------------------");
             System.out.println("Welche Spalte soll bei: " + datei1 + " verglichen werden?");
             String csv1_spalte4 = scanner.nextLine();
             System.out.println(csv1_spalte4 + " Spalte soll bei: " + datei2 + " mit ... verglichen werden: ");
             String csv2_spalte4 = scanner.nextLine();
-
+            System.out.println("-----------------------------------------------------------------------------");
             int[] auswertung = vergleich.ausführen(csv1_spalte1, csv1_spalte2, csv1_spalte3, csv1_spalte4, csv2_spalte1, csv2_spalte2, csv2_spalte3, csv2_spalte4);
 
             System.out.println("Anzahl der Zeilen für\t" + datei1 + "\t" + csv_allezeilen.size());
             System.out.println("Anzahl der Zeilen für\t" + datei2 + "\t" + csv2_allezeilen.size());
             System.out.println("-----------------------------------------------------------------------------");
-            System.out.println("Anzahl der richtigen Spalte\t" + csv1_spalte1 + "\t" + auswertung[0]);
-            System.out.println("Anzahl der richtigen Spalte\t" + csv1_spalte2 + "\t" + auswertung[1]);
-            System.out.println("Anzahl der richtigen Spalte\t" + csv1_spalte3 + "\t" + auswertung[2]);
-            System.out.println("Anzahl der richtigen Spalte\t" + csv1_spalte4 + "\t" + auswertung[3]);
+            System.out.println("Anzahl der richtigen Spalte\t" + csv1_spalte1 + " und " + csv2_spalte1 + "\t" + auswertung[0]);
+            System.out.println("Anzahl der richtigen Spalte\t" + csv1_spalte2 + " und " + csv2_spalte2 + "\t" + auswertung[1]);
+            System.out.println("Anzahl der richtigen Spalte\t" + csv1_spalte3 + " und " + csv2_spalte3 + "\t" + auswertung[2]);
+            System.out.println("Anzahl der richtigen Spalte\t" + csv1_spalte4 + " und " + csv2_spalte4 + "\t" + auswertung[3]);
             System.out.println("-----------------------------------------------------------------------------");
             System.out.println("Anzahl der gesamt richtigen: " + auswertung[4]);
         }
